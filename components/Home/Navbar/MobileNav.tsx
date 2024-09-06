279ff9902857d8883042817a92f9878dc5b290a8
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-09-07 02:04:02 +0530

    Complete Navbar Section🚩

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -8,12 +8,17 @@ type Props = {
 };
 
 const MobileNav = ({ closeNav, showNav }: Props) => {
+  const navOpen = showNav ? 'translate-x-0' : 'translate-x-[-100%]';
   return (
     <div>
       {/* Overlay */}
-      <div className="fixed inset-0 bg-black opacity-70 w-full h-screen z-[1000]">
+      <div
+        className={`${navOpen} transform transition-all duration-500  fixed inset-0 bg-black opacity-70 w-full h-screen z-[1000]`}
+      >
         {/* NavLink */}
-        <div className=" flex flex-col h-full w-[80%] sm:w-[60%]  fixed justify-center bg-[#0f0715] space-y-6 z-[10000] text-white ">
+        <div
+          className={`${navOpen} transform transition-all duration-500 delay-300 flex flex-col h-full w-[80%] sm:w-[60%]  fixed justify-center bg-[#0f0715] space-y-6 z-[10000] text-white `}
+        >
           {navLinks.map((navLinks) => {
             return (
               <Link key={navLinks.id} href={navLinks.url}>
@@ -23,9 +28,12 @@ const MobileNav = ({ closeNav, showNav }: Props) => {
               </Link>
             );
           })}
+          {/* Close */}
+          <CgClose
+            className="absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 h-6 w-6 cursor-pointer text-white"
+            onClick={closeNav}
+          />
         </div>
-        {/* Close */}
-        <CgClose className="absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 h-6 w-6 text-white" />
       </div>
     </div>
   );
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,7 +1,8 @@
+'use client';
 import { navLinks } from '@/constant/constant';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { HiBars3BottomRight } from 'react-icons/hi2';
 
 // define prop types
@@ -10,8 +11,28 @@ type Props = {
 };
 
 const Nav = ({ openNav }: Props) => {
+  const [navBg, setNavBg] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) {
+        setNavBg(true);
+      }
+      if (window.scrollY < 90) {
+        setNavBg(false);
+      }
+    };
+    window.addEventListener('scroll', handler);
+    return () => {
+      window.removeEventListener('scroll', handler);
+    };
+  }, []);
   return (
-    <div className="fixed h-[12vh] z-[10] bg-blue-950 w-full">
+    <div
+      className={`${
+        navBg ? 'bg-[#240b39]' : 'fixed'
+      } fixed h-[12vh] z-[10] w-full transition-all duration-200`}
+    >
       <div className="flex items-center justify-between h-full w-[95%] sm:w-[90%] xl:w-[80%] mx-auto">
         {/* Logo */}
         <Image

ace0c595dabaa5461f485ae05918a19ee00d63c0
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-09-17 00:26:09 +0530

    Fix config issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,34 @@
-import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
- 
+import { PluginAPI } from 'tailwindcss/types/config';
+import { default as flattenColorPalette } from 'tailwindcss/lib/util/flattenColorPalette';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  darkMode: "class",
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [
-    addVariablesForColors,
-  ],
+  plugins: [addVariablesForColors],
 };
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
-function addVariablesForColors({ addBase, theme }) {
-  const allColors = flattenColorPalette(theme("colors"));
-  const newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+function addVariablesForColors({ addBase, theme }: PluginAPI) {
+  const allColors = flattenColorPalette(theme('colors'));
+  const newVars: Record<string, string> = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => {
+      // Ensure `val` is a string
+      const value = typeof val === 'string' ? val : String(val);
+      return [`--${key}`, value];
+    }),
   );
- 
+
   addBase({
-    ":root": newVars,
+    ':root': newVars,
   });
 }

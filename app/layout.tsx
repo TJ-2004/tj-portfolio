684a7d52ab18b2197ced29c382222733fdfd6df0
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-09-17 01:16:10 +0530

    Added web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import ResponsiveNav from '@/components/Home/Navbar/ResponsiveNav';
 import Footer from '@/components/Home/Footer/Footer';
 import ScrollToTop from '@/components/Helper/ScrollToTop';
 import Update from '@/components/Helper/Update';
-// import AnimatedCursor from 'react-animated-cursor';
+import { Analytics } from '@vercel/analytics/react';
 
 const font = Sora({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
@@ -26,23 +26,9 @@ export default function RootLayout({
     <html lang="en" className="element">
       <body className={font.className}>
         <Update>
-          <div className="hidden md:block">
-            {/* <AnimatedCursor
-            innerSize={8}
-            outerSize={35}
-            innerScale={2}
-            outerScale={2}
-            outerAlpha={0}
-            innerStyle={{
-              backgroundColor: 'white',
-            }}
-            outerStyle={{
-              border: '3px solid white',
-            }}
-          /> */}
-          </div>
           <ResponsiveNav />
           {children}
+          <Analytics />
           <Footer />
           <ScrollToTop />
         </Update>
